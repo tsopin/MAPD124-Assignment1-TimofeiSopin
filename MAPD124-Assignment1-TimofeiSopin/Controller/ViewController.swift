4b68f6dff736d460738e8cc20503b9c345223fa6
Timofei Sopin <timofei.sopin@gmail.com>
2018-01-28 20:31:30 -0500

    Initial Commit

---
+++ MAPD124-Assignment1-TimofeiSopin/Controller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MAPD124-Assignment1-TimofeiSopin
+//
+//  Created by Timofei Sopin on 2018-01-28.
+//  Copyright © 2018 Centennial College. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

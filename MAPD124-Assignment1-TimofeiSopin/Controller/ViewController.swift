8399e78dbf5775858fcafbe313da932a2b65b0de
Timofei Sopin <timofei.sopin@gmail.com>
2018-01-28 21:34:37 -0500

    Design done, basic functionality added

--- MAPD124-Assignment1-TimofeiSopin/Controller/ViewController.swift
+++ MAPD124-Assignment1-TimofeiSopin/Controller/ViewController.swift
@@ -9,7 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var jackpotLabel: UILabel!
+    @IBOutlet weak var userMoneyLabel: UILabel!
+    @IBOutlet weak var currentBetLabel: UILabel!
+    
+    @IBOutlet weak var firstSlotImageView: UIImageView!
+    @IBOutlet weak var secondSlotImageView: UIImageView!
+    @IBOutlet weak var thirdSlotImageView: UIImageView!
+    
+    var firstSlot : Int = 0
+    var secondSlot : Int = 0
+    var thirdSlot : Int = 0
 
+    let slotArray = ["s1", "s2", "s3", "s4", "s5", "s6", "s7"]
+    
+    @IBAction func spinButton(_ sender: Any) {
+        
+        firstSlot = Int(arc4random_uniform(7))
+        secondSlot = Int(arc4random_uniform(7))
+        thirdSlot = Int(arc4random_uniform(7))
+        
+        firstSlotImageView.image = UIImage(named: slotArray[firstSlot])
+        secondSlotImageView.image = UIImage(named: slotArray[secondSlot])
+        thirdSlotImageView.image = UIImage(named: slotArray[thirdSlot])
+        
+    }
+    
+    
+    @IBAction func currentBetBtn(_ sender: Any) {
+    }
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
